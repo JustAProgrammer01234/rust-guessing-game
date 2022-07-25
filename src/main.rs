f8f8d3fc0ae2205f9478390582b679ba93958392
JustAProgrammer01234 <76458223+JustAProgrammer01234@users.noreply.github.com>
2022-07-25 19:44:58 +0800

    Starting to work on it.

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// bringing the io library to scope that comes from the standard library which is std.
+use std::io; 
+
+fn main() {
+    println!("Guessing game!");
+    println!("Guess the right number that's from 1-100. Good luck!"); 
+
+    loop {
+        println!("Please input your guess."); 
+        let mut guess = String::new(); 
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line."); 
+
+        println!("You guessed: {guess}"); 
+    }
+}

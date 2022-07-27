cbd8dc435ebe7c1e6d1e44514a6d917e924614ae
JustAProgrammer01234 <76458223+JustAProgrammer01234@users.noreply.github.com>
2022-07-27 18:11:25 +0800

    Almost there

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
-// bringing the io library to scope that comes from the standard library which is std.
+use std::cmp::Ordering; 
 use std::io; 
+
+use rand::Rng; 
 
 fn main() {
     println!("Guessing game!");
     println!("Guess the right number that's from 1-100. Good luck!"); 
 
+    let right_number = rand::thread_rng().gen_range(1..=100); 
+
     loop {
         println!("Please input your guess:"); 
+
         let mut guess = String::new(); 
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line."); 
 
-        println!("You guessed: {guess}"); 
+        let guess: u32 = guess.trim().parse().expect("Type a number brah."); 
+
+        match guess.cmp(&right_number) {
+            Ordering::Less => println!("It's too small!"), 
+            Ordering::Equal => { 
+                println!("You got the right answer! Congrats!"); 
+                break;
+            }, 
+            Ordering::Greater => println!("It's too big!")
+        }
     }
 }
